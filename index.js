8079eb807bf3a8fc8fef451f643d14cada694459
CL0UDH <smithy.weber.jeger.man.jensen@gmail.com>
2019-01-10 21:37:11 -0600

    Instalando graphiQL

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 import express from 'express';
+// graphql
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
 
 const app = express(); 
 
 app.get('/', (req, res) => {
     res.send('Todo listo');
 });
+
+// resolver
+const root = {hola: () => "Hola mundo desde graphQL"};
+
+app.use('/graphql', graphqlHTTP({
+    // que schema va a utilizar 
+    schema,
+    // el resolver se pasa como rootValue
+    rootValue: root,
+    // utilizar graphiql
+    graphiql: true
+}));
 
 app.listen(8000, () => console.log('El servidor esta funcionando'));

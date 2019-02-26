134acad77715908af8232bd703ef8eea6fe0f9ff
CL0UDH <smithy.weber.jeger.man.jensen@gmail.com>
2019-02-25 21:46:21 -0600

    Mostramos datos de consulta e integramos react-router-dom

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,8 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { ApolloProvider } from "react-apollo";
 import ApolloClient from "apollo-boost";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Importar componentes
 import Header from "./componentes/header";
 import Clientes from "./componentes/Clientes";
+import NuevoCliente from './componentes/NuevoCliente'
+import EditarCliente from './componentes/EditarCliente'
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
@@ -16,12 +21,22 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <Header/>
-        <Clientes/>
+        <Router>
+          <Fragment>
+            <Header />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Clientes}/>
+                <Route exact path="/cliente/editar/:id" component={EditarCliente}/>
+                <Route exact path="/cliente/nuevo" component={NuevoCliente}/>
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
       </ApolloProvider>
     );
   }
 }
 
 export default App;
-// TODO: Continuar en la seccion 11: 54. Mostrando los datos de la consulta
+// TODO: Continuar en la seccion 11: 56. Enlazando hacia el componente de editar cliente
--- cliente/src/componentes/Clientes.js
+++ cliente/src/componentes/Clientes.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Query } from "react-apollo";
 
 import { CLIENTES_QUERY } from "../queries/index";
@@ -12,7 +12,27 @@ const Conctactos = () => (
             console.log(data);
             
             return (
-                <h2 className="text-center">Listado de Clientes</h2>
+                <Fragment>
+                    <h2 className="text-center mt-4">Listado de Clientes</h2>
+                    <ul className="list-group mt-4">
+                        {data.getClientes.map(item => {
+                            return(
+                                <li key={item.id} className="list-group-item">
+                                    <div className="row justify-content-between align-items-center">
+                                        <div className="col-md-8 d-flex justify-content-between align-items-center">
+                                            {item.nombre} {item.apellido}
+                                        </div>
+                                        <div className="col-md-4 d-flex justify-content-end">
+                                            <a className="btn btn-success d-block d-md-inline-block">
+                                                Editar Cliente
+                                            </a>
+                                        </div>
+                                    </div>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </Fragment>
             )
         }}
     </Query>
